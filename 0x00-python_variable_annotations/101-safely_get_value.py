918541e228b9410bff065718fd4cb265bbf7dc10
mugishab2020 <mugishab2020@gmail.com>
2024-04-18 21:28:31 +0200

    now we know more about ytpevar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,6 +9,7 @@ T = TypeVar('T')
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Optional[T] = None) -> Union[Any, T]:
+    '''returnis the dataypes of these lists'''
     if key in dct:
         return dct[key]
     else:

8d96b69aa9eb2ea68475bb7026f2968211a37958
mugishab2020 <mugishab2020@gmail.com>
2024-04-18 21:27:28 +0200

    now we know more about ytpevar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,13 +4,11 @@
 from typing import Union, TypeVar, Optional, Mapping, Any
 
 
-T= TypeVar('T')
+T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Optional[T] = None) -> Union[Any, T]:
-
-   
     if key in dct:
         return dct[key]
     else:

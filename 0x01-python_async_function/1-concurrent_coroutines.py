e5b1e5d297b5a0fbc0dc07a25e7f79e9aa8377e8
mugishab2020 <mugishab2020@gmail.com>
2024-04-22 18:20:20 +0200

    We are trying

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' this si to use asyncio.as-complited method'''
+
+
+from typing import List
+import asyncio
+
+new_wait = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    a = [new_wait(max_delay) for _ in range(n)]
+    return [await a for a in asyncio.as_completed(a)]
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/python3
+
+
+import time
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    '''measures the total execution
+    '''
+    a = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    delay = (time.perf_counter() - a) / n
+    return delay

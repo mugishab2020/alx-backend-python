1b6693474ab3892c50cb45673d3fe5f39ddb0895
mugishab2020 <mugishab2020@gmail.com>
2024-04-22 21:50:20 +0200

    we are good to go

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,5 +9,7 @@ new_wait = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> float:
+    '''We are taking the max_delay and n as arguments
+    and spawn wait_random n times'''
     a = [new_wait(max_delay) for _ in range(n)]
     return [await a for a in asyncio.as_completed(a)]

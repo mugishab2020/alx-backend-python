b14ecac8507756fbc19632c8e78dbf64b014b5e4
mugishab2020 <mugishab2020@gmail.com>
2024-04-22 21:57:10 +0200

    We are trying

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,9 +9,12 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''takes in 2 int arguments n and max_delay and
-       spawn wait_random n times with the
+    '''takes in n and max_delay and
+       spawn task_wait_random n times with the
        specified max_delay
     '''
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    delays = [wait_random(max_delay) for _ in range(n)]
+    completed_delays = []
+    for coro in asyncio.as_completed(delays):
+        completed_delays.append(await coro)
+    return completed_delays

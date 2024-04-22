bca1a4e0c762a64bd19d94dc551a0015f1cd5c63
mugishab2020 <mugishab2020@gmail.com>
2024-04-22 13:38:19 +0200

    Now we know uniform method from random

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+import asyncio
+from random import uniform
+
+
+'''
+this is the definition of the async function the delays random time'''
+
+async def wait_random(max_delay: int =10) -> float:
+    '''
+    we used the unifom method to get the random delay 
+    and we specify the startig point (0) and the endig point (max_delay)
+    '''
+
+    new_delay = uniform(0, max_delay)
+
+    await asyncio.sleep(new_delay)
+    return new_delay

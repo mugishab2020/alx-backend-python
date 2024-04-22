3f615929b9f92a185fd4087ce8e14b3c3221a4cb
mugishab2020 <mugishab2020@gmail.com>
2024-04-22 21:51:25 +0200

    we are good to go

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env/python3
-'''This is for using the  time package'''
-
-
-import time
+#!/usr/bin/env python3
+''' The basics of async
+'''
+from typing import List
 import asyncio
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    '''measures the total execution
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''takes in 2 int arguments n and max_delay and
+       spawn wait_random n times with the
+       specified max_delay
     '''
-    a = time.perf_counter()
-    asyncio.run(wait_n(n, max_delay))
-    delay = (time.perf_counter() - a) / n
-    return delay
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]

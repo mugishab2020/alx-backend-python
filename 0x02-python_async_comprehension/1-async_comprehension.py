d5711d7c1274296af8e79e83baac8256d074e243
mugishab2020 <mugishab2020@gmail.com>
2024-04-23 11:12:58 +0200

    We got this

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' this is the continuation to the task one'''
+
+
+from typing import List
+import asyncio
+new_task = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> list[float]:
+    '''this is an async comprehension'''
+    new_list = []
+
+    async for i in new_task():
+        new_list.append(i)
+    return new_list
